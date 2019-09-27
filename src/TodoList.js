6d6ac3517dd076ffd4645e742a89ac2124c21c80
gong zhenhua <nestmilk@live.com>
2019-09-27 13:59:19 +0800

    change

---
+++ src/TodoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// function App() {
+//   return (
+//     <div>
+//       hello, i am dell
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+    render () {
+        return (
+            <div>
+                hello, i am class component.
+            </div>
+        )
+    }
+}
+
+export default App;
